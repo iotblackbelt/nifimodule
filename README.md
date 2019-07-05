ca8a9073a41ec6b4be6ab324b027341d33c05f19
Eric van Uum <evanuum@users.noreply.github.com>
2019-07-05 14:22:34 +0200

    Added BackupFlow direct method and bug fixes

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ Go to the Azure potal and open up the IoT Edge device you created in the first s
 
 ![Set module](images/Set_module.png "Set module")
 
-Give your module a name (f.i. NifiModule) and use "gbbiotwesouth/nifimodule:0.1.5-amd64" as the Image URI.
+Give your module a name (f.i. NifiModule) and use "gbbiotwesouth/nifimodule:0.2.0-amd64" as the Image URI.
 Use the following container create options:
 ```json
 {
@@ -99,8 +99,14 @@ Use the following settings for the StandardSSLContextService:
 
 If everything was executed correctly, the module will have stored the provided certificate in the Java JDK keystore and will now be used to communicate with th IoT Edge Hub.
 
-If you want the flow to actually send some information, you have to create another processor that sends its data to the PublishMQTT processor. In my flow I added a GenerateFlowFile, set the interval to 5 seconds and connected it to the PublishMQTT.
-You can use the Azure IoT Hub extension in Visual Studio Code to start monitoring the messages that come from your IoT Edge device. If you've set up everything correctly you should see the message somming in a messages form an IoT Edge module.
+If you want the flow to actually send some information, you have to create another processor that sends its data to the PublishMQTT processor. In my flow I added a GenerateFlowFile, set the interval to 5 seconds and connected it to the PublishMQTT. You can use the Azure IoT Hub extension in Visual Studio Code to start monitoring the messages that come from your IoT Edge device. If you've set up everything correctly you should see the message somming in a messages form an IoT Edge module.
+
+## Backup Nifi Flow file
+The Nifi module also implements a direct method to backup the current flow file to Azure storage. The direct method "BackupFlow" requires a request object that contains the version:
+```json
+{"version":"<your version>"}
+```
+The direct method can be executed from the Azure portal. The flow file is copied locally on the IoT Edge device as well as uploaded to blob storage.
 
 ![Monitor messages](images/Monitor.png "Monitor messages")
 
--- modules/NifiModule/app.js
+++ modules/NifiModule/app.js
@@ -2,16 +2,17 @@
 
 var Transport = require('azure-iot-device-mqtt').Mqtt;
 var Client = require('azure-iot-device').ModuleClient;
-const { exec } = require('child_process');
+var moduleTwin;
+const { exec, execSync } = require('child_process');
 
 // Blob storage
 const azureStorage = require('azure-storage');
 const path = require('path');
  
 const azureStorageConfig = {
-    accountName: "",
-    accountKey: "",
-    containerName: ""
+    accountName: '',
+    accountKey: '',
+    containerName: ''
 };
 
 // Initiate the module client
@@ -28,15 +29,16 @@ Client.fromEnvironment(Transport, function (err, client) {
       if (err) {
         throw err;
       } else {
-        console.log('IoT Hub module client initialized');
+        console.log('[INFO] IoT Hub module client initialized');
 
         // Act on twin change of the module.
         client.getTwin(function (err, twin) {
           if (err) {
-              console.error('Error getting twin: ' + err.message);
+              console.error('[ERROR] Error getting twin: ' + err.message);
           } else {
-            twin.on('properties.desired', function(data) {
-              console.log("Module Twin: %s", JSON.stringify(data));
+            moduleTwin = twin;
+            moduleTwin.on('properties.desired', function(data) {
+              console.log('Module Twin: %s', JSON.stringify(data));
               // get the twin information
               var flowversion = data.flowversion; 
               var certificatefile = data.certificatefile;
@@ -50,30 +52,71 @@ Client.fromEnvironment(Transport, function (err, client) {
 
               // Restart NiFi
               restartNifi();
+
+              // Report back status
+              var reported = { flowversion: flowversion };
+              sendReportedProperties(reported);
             });
           }
+        });
+
+        // Register handler for 'BackupFlow'
+        client.onMethod('BackupFlow', function (request, response) {
+          console.log('[INFO] Received a request for BackupFlow()');
+          console.log('[INFO] ' + JSON.stringify(request.payload, null, 2));
+          var responsePayload = {
+            message: 'Unknown'
+          };
+
+          if (request.payload.version)
+          {
+            // Copy the flow file and upload it to blob storage
+            responsePayload.message = backupFlowfile(request.payload.version);
+          }
+          else {
+            responsePayload.message = '[ERROR] Flow file version not set.';
+          }
+
+          // Send repsonse
+          response.send(200, responsePayload, function (err) {
+            if (err) {
+              console.error('[ERROR] Unable to send method response: ' + err.toString());
+            } else {
+              console.log('[INFO] Response to BackupFlow() sent.');
+            }
+          });
         });
       }
     });
   }
 });
 
+// Send the reported properties patch to the hub
+function sendReportedProperties(data) {
+  moduleTwin.properties.reported.update(data, function(err) {
+    if (err) throw err;
+    console.log('[INFO] Twin state reported');
+    console.log('[INFO] ' + JSON.stringify(data, null, 2));
+  });
+}
+
 function setCertificatefile(file) {
   // Laoding certificate file
-  console.log("Loading certificate file.");
+  console.log('[INFO] Loading certificate file.');
   if (file) {
-    downloadBlob(file, '/config/');
-    exec('keytool -import -noprompt -alias iot-edge -file /config/' + file + ' -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit', (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error('Error loading certificate: ' + err.message);
+    var download = downloadBlob(file, '/config/');
+    download.then(function(){
+      try {
+        execSync('keytool -import -noprompt -alias iot-edge -file /config/' + file + ' -keystore /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/security/cacerts -storepass changeit');
+        console.log('[INFO] Loading certificate successful.');
       }
-      else {
-          // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
+      catch (err){
+        console.error('[ERROR] Error loading certificate: ' + err.message);
       }
-    });
+    })
+    .catch(function (err){
+      console.error('[ERROR] Error downloading flow file: ' + err.message);
+    })
   }
 }
 
@@ -81,49 +124,109 @@ function setFlowfile(version){
   // Load flow file from /config directory (can also be done from blob storage online)
   if (version)
   {
-    console.log("Loading Nifi flow file.");
-    downloadBlob(version + '.flow.xml.gz', '/config/');
-    exec('cp /config/' + version + '.flow.xml.gz /nifi-1.9.2/conf/flow.xml.gz', (err, stdout, stderr) => {
-      if (err) {
-        // node couldn't execute the command
-        console.error('Error loading flow: ' + err.message);
+    console.log('[INFO] Loading Nifi flow file.');
+    var download = downloadBlob(version + '.flow.xml.gz', '/config/');
+    download.then(function(){
+      try {
+        execSync('cp /config/' + version + '.flow.xml.gz /nifi-1.9.2/conf/flow.xml.gz');
+        console.log('[INFO] Loading flow file successful.');
       }
-      else {
-          // the *entire* stdout and stderr (buffered)
-        console.log(`stdout: ${stdout}`);
-        console.log(`stderr: ${stderr}`);
+      catch (err){
+        console.error('[ERROR] Error loading flow file: ' + err.message);
       }
-    });
+    })
+    .catch(function (err){
+      console.error('[ERROR] Error downloading flow file: ' + err.message);
+    })
+  }
+}
+
+function backupFlowfile(version){
+  // Load flow file from /config directory (can also be done from blob storage online)
+  if (version)
+  {
+    console.log('[INFO] Saving Nifi flow file.');
+    try {
+      execSync('cp /nifi-1.9.2/conf/flow.xml.gz /config/' + version + '.flow.xml.gz');
+      console.log('[INFO] Saving flow file successful.');
+      var upload = uploadBlob(version + '.flow.xml.gz', '/config/');
+      upload.then(function () {
+          console.log('[INFO] Uploading flow file successful.');
+        }
+      )
+      .catch(function(err){
+          console.log('[INFO] Uploading flow file failed. ' + err.message);
+        }
+      )
+    }
+    catch (err){
+      console.error('[ERROR] Error backup flow file: ' + err.message);
+    }
   }
 }
 
 function restartNifi(){
   // (re)start NiFi
-  console.log("(Re)Starting NiFi.");
-  exec('/nifi-1.9.2/bin/nifi.sh restart', (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      console.error('Error starting Apache Nifi: ' + err.message);
-    }
-    else {              
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    }
-  });
+  console.log('[INFO] (Re)Starting NiFi.');
+  try {
+    exec('/nifi-1.9.2/bin/nifi.sh restart', (err, stdout, stderr) => {
+      if (err)
+        console.error('[ERROR] Error starting Nifi: ' + err.message);
+      else
+        console.log('[INFO] Started Nifi successful.');
+    });
+   }
+  catch (err){
+    console.error('[ERROR] Error starting Nifi: ' + err.message);
+  }
 }
 
 function downloadBlob (blobName, downloadFilePath) {
   // only download if storage information is set
-  if (azureStorageConfig.accountName && azureStorageConfig.accountKey && azureStorageConfig.containerName)
-  {
-    const name = path.basename(blobName);
-    console.log("File to download: %s", name);
-    const blobService = azureStorage.createBlobService(azureStorageConfig.accountName, azureStorageConfig.accountKey); 
-    blobService.getBlobToLocalFile(azureStorageConfig.containerName, blobName, `${downloadFilePath}${name}`, function(error, serverBlob) {
+  return new Promise(function(resolve, reject) {
+    if (azureStorageConfig.accountName && azureStorageConfig.accountKey && azureStorageConfig.containerName)
+    {
+      const name = path.basename(blobName);
+      console.log('[INFO] File to download: %s', name);
+      const blobService = azureStorage.createBlobService(azureStorageConfig.accountName, azureStorageConfig.accountKey);
+      blobService.getBlobToLocalFile(azureStorageConfig.containerName, blobName, `${downloadFilePath}${name}`, function(error, serverBlob) {
         if (error) {
-            console.log(error);
+          reject(error);
         }
-    });
-  }
+        else
+          console.log('[INFO] File downloaded: %s', name);
+          resolve();
+      });
+    }
+    else
+    {
+      console.log('[WARN] Azure storage credentials are not set. Please update module twin desired properties.');
+      resolve();
+    }
+  });
+};
+
+function uploadBlob (blobName, uploadFilePath) {
+  // only upload if storage information is set
+  return new Promise(function(resolve, reject) {
+    if (azureStorageConfig.accountName && azureStorageConfig.accountKey && azureStorageConfig.containerName)
+    {
+      const name = path.basename(blobName);
+      console.log('[INFO] File to upload: %s', name);
+      const blobService = azureStorage.createBlobService(azureStorageConfig.accountName, azureStorageConfig.accountKey); 
+      blobService.createBlockBlobFromLocalFile(azureStorageConfig.containerName, blobName, `${uploadFilePath}${name}`, function(error) {
+          if (error) {
+            reject(error);
+          }
+          else {
+            resolve();
+          }
+      });
+    }
+    else
+    {
+      console.log('[WARN] Azure storage credentials are not set. Please update module twin desired properties.');
+      resolve();
+    }
+  });
 };
